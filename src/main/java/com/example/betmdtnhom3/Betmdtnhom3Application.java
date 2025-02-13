bde48b318bd901e5477d04d90db253beb317ba44
quanganh2003aaa <phamquanganh20122003@gmail.com>
2025-02-13 15:23:09 +0700

    first commit

---
+++ src/main/java/com/example/betmdtnhom3/Betmdtnhom3Application.java
@@ -0,0 +1,13 @@
+package com.example.betmdtnhom3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Betmdtnhom3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Betmdtnhom3Application.class, args);
+	}
+
+}

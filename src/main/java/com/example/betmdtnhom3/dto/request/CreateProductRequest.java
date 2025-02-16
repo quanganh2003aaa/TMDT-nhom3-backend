59c37b65c60b97e2041b135a3d15be8124d0be0d
quanganh2003aaa <phamquanganh20122003@gmail.com>
2025-02-16 14:37:10 +0700

    CreateProductRequest

--- src/main/java/com/example/betmdtnhom3/dto/request/CreateProductRequest.java
+++ src/main/java/com/example/betmdtnhom3/dto/request/CreateProductRequest.java
@@ -3,10 +3,74 @@ package com.example.betmdtnhom3.dto.request;
 public class CreateProductRequest {
     private String id;
     private String name;
-    private String img;
     private String size;
     private int price;
     private int quantity;
     private String description;
     private int category;
+    private int brand;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getBrand() {
+        return brand;
+    }
+
+    public void setBrand(int brand) {
+        this.brand = brand;
+    }
 }

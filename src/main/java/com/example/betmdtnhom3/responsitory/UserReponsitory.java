b592f60a0bfe064bdb4dcfb28ffd86daa7d2679a
quanganh2003aaa <phamquanganh20122003@gmail.com>
2025-03-17 16:22:16 +0700

    list user

--- src/main/java/com/example/betmdtnhom3/responsitory/UserReponsitory.java
+++ src/main/java/com/example/betmdtnhom3/responsitory/UserReponsitory.java
@@ -1,10 +1,12 @@
 package com.example.betmdtnhom3.responsitory;
 
+import com.example.betmdtnhom3.Enum.Role;
 import com.example.betmdtnhom3.entity.User;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +16,5 @@ public interface UserReponsitory extends JpaRepository<User, String> {
     Optional<User> findByTel(String tel);
 
     Optional<User> findById(String id);
+    List<User> findAllByRole(Role role);
 }
